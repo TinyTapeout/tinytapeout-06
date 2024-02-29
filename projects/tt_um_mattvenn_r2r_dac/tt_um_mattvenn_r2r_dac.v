6b3c7d5a779eb482ce9ea730c424ecb2009bd3d4
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-02-29 12:57:10 +0200

    feat: update project tt_um_mattvenn_r2r_dac from mattvenn/tt06-analog-r2r-dac
    Commit: 975d769ae7b5467309c11e208f70c73849f3c99c
    Workflow: https://github.com/mattvenn/tt06-analog-r2r-dac/actions/runs/8094856947

--- projects/tt_um_mattvenn_r2r_dac/tt_um_mattvenn_r2r_dac.v
+++ projects/tt_um_mattvenn_r2r_dac/tt_um_mattvenn_r2r_dac.v
@@ -15,5 +15,36 @@ module tt_um_mattvenn_r2r_dac (
     input  wire       clk,
     input  wire       rst_n
 );
+    wire [7:0] r2r_out;
+
+    // ties for the output enables
+    assign uio_oe[0] = VGND;
+    assign uio_oe[1] = VGND;
+//  assign uio_oe[2] = VGND;
+
+    r2r_dac_control r2r_dac_control(
+        .clk(clk),                  // expect a 10M clock
+        .n_rst(rst_n),
+        .ext_data(uio_in[0]),       // if this is high, then DAC data comes from ui_in[7:0]
+        .load_divider(uio_in[1]),   // load value set on data to the clock divider
+        .data(ui_in),               // connect to ui_in[7:0]
+        .r2r_out(r2r_out),          // 8 bit out to the R2R DAC
+        .VPWR(VPWR),
+        .VGND(VGND)
+        );
+
+    r2r r2r(
+        .b0(r2r_out[0]),
+        .b1(r2r_out[1]),
+        .b2(r2r_out[2]),
+        .b3(r2r_out[3]),
+        .b4(r2r_out[4]),
+        .b5(r2r_out[5]),
+        .b6(r2r_out[6]),
+        .b7(r2r_out[7]),
+        .out(ua[0]),
+        .VSUBS(VGND),
+        .GND(VGND)
+        );
 
 endmodule
