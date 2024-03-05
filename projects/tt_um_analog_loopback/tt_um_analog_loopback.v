f5129ca656515836e292392914e13d452dd8497f
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2024-03-05 19:34:57 +0200

    feat: update project tt_um_analog_loopback from TinyTapeout/tt06-analog-loopback
    Commit: a9025f33358baabe9ca54efaab5a0ac07f5600cb
    Workflow: https://github.com/TinyTapeout/tt06-analog-loopback/actions/runs/8160526388

--- projects/tt_um_analog_loopback/tt_um_analog_loopback.v
+++ projects/tt_um_analog_loopback/tt_um_analog_loopback.v
@@ -16,10 +16,45 @@ module tt_um_analog_loopback (
     input  wire       rst_n
 );
 
-    assign uo_out = VGND;
-    assign uio_out = VGND;
-    assign uio_oe = VGND;
+    wire tie_low;
+    // must have at least once cell in the verilog or netgen has a problem
+    sky130_fd_sc_hd__conb_1 sky130_fd_sc_hd__conb_1 (
+        .VGND(VGND),
+        .VNB (VGND),
+        .VPB (VPWR),
+        .VPWR(VPWR),
+        .LO  (tie_low)
+    );
+
+    // all ports have to be assigned individually or netgen has a problem
+    assign uo_out[0] = tie_low;
+    assign uo_out[1] = tie_low;
+    assign uo_out[2] = tie_low;
+    assign uo_out[3] = tie_low;
+    assign uo_out[4] = tie_low;
+    assign uo_out[5] = tie_low;
+    assign uo_out[6] = tie_low;
+    assign uo_out[7] = tie_low;
+
+    assign uio_out[0] = tie_low;
+    assign uio_out[1] = tie_low;
+    assign uio_out[2] = tie_low;
+    assign uio_out[3] = tie_low;
+    assign uio_out[4] = tie_low;
+    assign uio_out[5] = tie_low;
+    assign uio_out[6] = tie_low;
+    assign uio_out[7] = tie_low;
+
+    assign uio_oe[0] = tie_low;
+    assign uio_oe[1] = tie_low;
+    assign uio_oe[2] = tie_low;
+    assign uio_oe[3] = tie_low;
+    assign uio_oe[4] = tie_low;
+    assign uio_oe[5] = tie_low;
+    assign uio_oe[6] = tie_low;
+    assign uio_oe[7] = tie_low;
     
+    // the loopback
     assign ua[1] = ua[0];
     assign ua[3] = ua[2];
     assign ua[5] = ua[4];
